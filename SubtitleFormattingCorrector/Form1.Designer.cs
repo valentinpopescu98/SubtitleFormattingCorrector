c240a0bdd2e391243bfbc5eddd2a90fad1934df4
Valentin <valentinpopescu98@gmail.com>
2021-04-12 16:47:55 +0300

    Functional

---
+++ SubtitleFormattingCorrector/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace SubtitleLanguageSwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.quitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(358, 38);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(84, 23);
+            this.browseBtn.TabIndex = 0;
+            this.browseBtn.Text = "Browse file...";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(40, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(720, 250);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(200, 410);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "CONVERT";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // quitBtn
+            // 
+            this.quitBtn.Location = new System.Drawing.Point(525, 410);
+            this.quitBtn.Name = "quitBtn";
+            this.quitBtn.Size = new System.Drawing.Size(75, 23);
+            this.quitBtn.TabIndex = 3;
+            this.quitBtn.Text = "QUIT";
+            this.quitBtn.UseVisualStyleBackColor = true;
+            this.quitBtn.Click += new System.EventHandler(this.quitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.quitBtn);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.browseBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button quitBtn;
+    }
+}
+
---
+++ SubtitleFormattingCorrector/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SubtitleLanguageSwapper
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        private string text;
+        string file;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                file = openFileDialog1.FileName;
+                try
+                {
+                    text = File.ReadAllText(file);
+                    richTextBox1.AppendText(text);
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+
+            StringReader input = new StringReader(text);
+            string result = "";
+
+            // matches a timestamp line with a "->" and no alpha characters
+            Regex timestampRegex = new Regex(@"[^A-Za-z]*-\s*>[^A-Za-z]*");
+
+            string line;
+            while ((line = input.ReadLine()) != null)
+            {
+                // if a timestamp line is found then it is modified
+                if (timestampRegex.IsMatch(line))
+                {
+                    line = Regex.Replace(line, @"\s", ""); // remove all whitespace
+                    line = line.Replace("-", ""); // remove excessive hyphens
+                    line = line.Replace(">", " --> "); // update arrow style
+                    line = line.Replace(".", ","); // update floating point format
+                }
+
+                result += (line + "\n");
+            }
+
+            richTextBox1.AppendText(result);
+            StreamWriter streamWriter = File.CreateText(file);
+            streamWriter.Write(result);
+        }
+
+        private void quitBtn_Click(object sender, EventArgs e)
+        {
+            if (Application.MessageLoop)
+            {
+                // WinForms app
+                Application.Exit();
+            }
+            else
+            {
+                // Console app
+                Environment.Exit(1);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
